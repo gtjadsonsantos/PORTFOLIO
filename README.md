e9cce5f814bb07a366c36a89c0b97b8ab67bb0db
jadson179 <jadson44.santos@gmail.com>
2020-04-18 18:23:09 -0300

    Adicionado funcionalidade de emissao de notificaçao, quando um usuario acessar o website

--- README.md
+++ README.md
@@ -145,6 +145,15 @@ navegue até src/Utils/config.js, nesse arquivo terá o um objeto de configuraç
         role: "CARGO ATUAL",
         icon: "URL DO FAVICON",
         name: "SEU NOME"
+    },
+    platforms: {
+      slack: {
+          url: "URL WEBHOOK SLACK",
+          channel: "NOME DO CANAL",
+          username: "NOME DO APP",
+          text: "MENSAGEM QUE QUER RECEBER",
+          icon_emoji: "CODIGO DO EMOJI EMOJI" 
+      }
     }
 }
 ```
---
+++ build/precache-manifest.da451f124baa60be76d4d65565d04a1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3553b808ede2e654a9306c44e58ac635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ace1566a5ebced0f124",
+    "url": "/static/css/main.6fcb9950.chunk.css"
+  },
+  {
+    "revision": "f925a95b94d12b648fc9",
+    "url": "/static/js/2.6487f767.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6487f767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ace1566a5ebced0f124",
+    "url": "/static/js/main.5ea1578a.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
